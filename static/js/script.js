16861d27e8271b6594782c9544c840811efbe265
Mohammad Jamiu <balogunmohammedjamiu@gmail.com>
2022-03-10 20:10:11 -0800

    update

--- static/js/script.js
+++ static/js/script.js
@@ -4,17 +4,20 @@ const lineTwo = document.querySelector(".menu-line-two");
 const lineThree = document.querySelector(".menu-line-three");
 const seeAnswer = document.querySelector(".see-answer");
 const hideShowImg = document.querySelector(".hide-show-image");
+const logoTextNone = document.querySelector(".logo-text");
 
 const nav = document.querySelector("header nav");
+const logoText = document.querySelector("header tspan");
 
 harmburger.addEventListener("click", () => {
-  console.log("hello");
+  // console.log("hello");
 
   lineOne.classList.toggle("line-one");
 
   lineThree.classList.toggle("line-three");
   lineTwo.classList.toggle("line-two");
   nav.classList.toggle("nav-active");
+  logoText.classList.toggle("logo-text-none");
 });
 
 seeAnswer.addEventListener("click", () => {
